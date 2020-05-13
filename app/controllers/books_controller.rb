fa37a73e1dea231bfbd42be1616a7b24b01e9652
vagrant <vagrant@localhost.localdomain>
2020-05-13 16:51:15 +0000

    Update createサクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+       flash[:notice] = "Book was successfully created."
        redirect_to book_path(@book)
    else
    	   render "new"
