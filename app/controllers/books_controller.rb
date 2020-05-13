18a82283622e8c097166a06dc91a6ee91a215df3
vagrant <vagrant@localhost.localdomain>
2020-05-13 16:38:29 +0000

    バリデーションチェック途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+       redirect_to book_path(@book)
+   else
+   	   render "new"
+   	end
   end
 
   def edit
