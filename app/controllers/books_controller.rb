4b79ceb15d5cc3089f349332e8c569a33c30c192
vagrant <vagrant@localhost.localdomain>
2020-05-13 10:58:32 +0000

    Update updateアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,12 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(book)
+end
+
   private
     def book_params
       params.require(:book).permit(:title, :body)
