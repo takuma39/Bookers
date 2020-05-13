b45b3932137afc7fdcac9c4b9661d14d1312cd7c
vagrant <vagrant@localhost.localdomain>
2020-05-13 10:38:36 +0000

    Update show.html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to book_path
+    redirect_to books_path(book.id)
   end
 
   def edit
