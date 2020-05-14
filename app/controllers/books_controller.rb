4c3c37965112e7f4fdbea10fbca0235b174b5175
vagrant <vagrant@localhost.localdomain>
2020-05-14 03:57:02 +0000

    Update destroyボタン　＆　サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,8 +39,10 @@ end
 
 def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.destroy
+       flash[:notice] = "Book was successfully destroyed."
+       redirect_to books_path
+   end
 end
 
   private
